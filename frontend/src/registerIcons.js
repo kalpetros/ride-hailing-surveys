15763bc5cd617d1a9228db12647a522a24c0aca1
Petros Kalogiannakis <github@petroskal.com>
2021-04-02 17:38:03 +0300

    Add register icons util

---
+++ frontend/src/registerIcons.js
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+export default function registerIcons() {
+  library.add(faStar);
+}
