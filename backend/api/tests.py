00d2725f6cc7d6d3078c0ed2f9c26f5e1049eefa
Petros Kalogiannakis <github@petroskal.com>
2021-04-02 18:51:41 +0300

    Add feedback testcase

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from api.models import Feedback
+from api.models import Passenger
+
+
+class FeedbackTestCase(APITestCase):
+    def setUp(self):
+        passenger = Passenger(
+            first_name='John',
+            last_name='Doe',
+            email='test@test.com'
+        )
+
+        passenger.save()
+
+    def test_create_feedback(self):
+        """
+        Check for feedback creation
+        """
+        url = 'http://localhost:8000/api/v1/feedback/'
+        data = {
+            "rating": "4",
+            "survey": "",
+            "user": "test@test.com",
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Feedback.objects.count(), 1)
+
+        feedback = Feedback.objects.last()
+        rating = feedback.rating
+        self.assertEqual(rating, '4')
